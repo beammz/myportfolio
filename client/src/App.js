3157f69b2ab84c986063442b32622b1c0d4cec78
Beatriz <beammz@gmail.com>
2021-12-16 13:32:29 -0300

    :sparkles: adding social media icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import Profile from './PortfolioContainer/Home/Profile';
 
 function App() {
   return (
     <div className="App">
+      <Profile/>
     </div>
   );
 }
---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href='https://www.linkedin.com/in/beatriz-mazieri/'>
+                            <i className='fa fa-linkedin-square'></i>
+                        </a>
+                        <a href='https://github.com/beammz'>
+                            <i className='fa fa-github-square'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-google-plus-square'></i>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
